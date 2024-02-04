1e45240fda6ffc4b9ad912465968ee43ef323546
Andrejs Sikorskis <as283@alu.ua.es>
2024-02-04 18:15:41 +0100

    Project skeleton to test composer packaging

---
+++ src/PlantUmlProcessor.php
@@ -0,0 +1,14 @@
+<?php
+
+class PlantUmlProcessor
+{
+    public function parse(string $schema): void
+    {
+        return;
+    }
+
+    public function serialize($schema): string
+    {
+        return '';
+    }
+}
