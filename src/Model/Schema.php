6d6dff6a1797d99cd22b6e481e4abe9084d5e2a4
Andrejs Sikorskis <as283@alu.ua.es>
2024-03-22 17:06:27 +0100

    Initialized class and relation arrays in Schema

--- src/Model/Schema.php
+++ src/Model/Schema.php
@@ -2,20 +2,22 @@
 
 namespace As283\PlantUmlProcessor\Model;
 
-class Schema 
+class Schema
 {
     /**
      * @var Relation[]
      */
-    public $relations;
+    public $relations = [];
 
     /**
      * Key is the class name and value is the ClassMetadata
      * @var array<string,ClassMetadata>
      */
-    public $classes;
+    public $classes = [];
 
-    public function __construct(){}
+    public function __construct()
+    {
+    }
 
     /**
      * @param ClassMetadata[] $classes
@@ -31,18 +33,18 @@ class Schema
         foreach ($schema->classes as $classname => $class) {
             $i = 0;
             foreach ($schema->relations as $relation) {
-                if($relation->from[0] === $classname){
-                    if(!array_key_exists($relation->to[0], $class->relatedClasses)){
+                if ($relation->from[0] === $classname) {
+                    if (!array_key_exists($relation->to[0], $class->relatedClasses)) {
                         $class->relatedClasses[$relation->to[0]] = [];
                     }
                     $class->relatedClasses[$relation->to[0]][] = $i;
-                } else if($relation->to[0] === $classname){
-                    if(!array_key_exists($relation->from[0], $class->relatedClasses)){
+                } else if ($relation->to[0] === $classname) {
+                    if (!array_key_exists($relation->from[0], $class->relatedClasses)) {
                         $class->relatedClasses[$relation->from[0]] = [];
                     }
                     $class->relatedClasses[$relation->from[0]][] = $i;
                 }
-                
+
                 $i++;
             }
         }
